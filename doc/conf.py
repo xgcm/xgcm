6b2d49bf4af02a9f132bd8656a4eb699394d0c27
Ryan Abernathey <rpa@ldeo.columbia.edu>
2016-01-31 18:02:17 -0500

    more trying to fix rtd

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,11 @@ import xgcm
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(os.path.abspath('..'))
+
+print "python exec:", sys.executable
+print "sys.path:", sys.path
+
 
 # -- General configuration ------------------------------------------------
 
