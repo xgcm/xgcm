e22e3216969ff1eca3bc94d052a44f990a7e56da
Ryan Abernathey <rpa@ldeo.columbia.edu>
2016-10-10 23:31:18 -0400

    so close to llc

--- xgcm/models/mitgcm/mds_store.py
+++ xgcm/models/mitgcm/mds_store.py
@@ -8,6 +8,7 @@ from glob import glob
 import os
 import numpy as np
 import xarray as xr
+import dask.array as da
 
 # we keep the metadata in its own module to keep this one cleaner
 from .variables import dimensions, \
@@ -20,7 +21,7 @@ from .variables import dimensions, \
 from .utils import parse_meta_file, read_mds, parse_available_diagnostics
 
 # should we hard code this?
-LLC_NUM_FACES=13
+LLC_NUM_FACES = 13
 LLC_FACE_DIMNAME = 'face'
 
 def open_mdsdataset(dirname, iters='all', prefix=None, read_grid=True,
@@ -629,9 +630,57 @@ def _reshape_for_llc(dims, data):
             # add face dimension to dims
             jdim = dims.index(dim)
             dims.insert(jdim, LLC_FACE_DIMNAME)
-            # reshape data
-            newshape = list(data.shape)
-            newshape[jdim] = newshape[jdim]/LLC_NUM_FACES
-            newshape.insert(jdim, LLC_NUM_FACES)
-            data = data.reshape(newshape)
+            data = _reshape_llc_data(data, jdim)
     return dims, data
+
+
+def _reshape_llc_data(data, jdim):
+    """Fix the weird problem with llc data array order."""
+    # Can we do this without copying any data?
+    # If not, we need to go upstream and implement this at the MDS level
+    # Or can we fudge it with dask?
+    # this is all very specific to the llc file output
+    # would be nice to generalize more, but how?
+    nside = data.shape[jdim] / LLC_NUM_FACES
+    # how the LLC data is laid out along the j dimension
+    strides = ((0,3), (3,6), (6,7), (7,10), (10,13))
+    # whether to reshape each face
+    reshape = (False, False, False, True, True)
+    # this will slice the data into 5 facets
+    slices = [jdim * (slice(None),) + (slice(nside*st[0], nside*st[1]),)
+              for st in strides]
+    facet_arrays = [data[sl] for sl in slices]
+    face_arrays = []
+    for ar, rs, st in zip(facet_arrays, reshape, strides):
+        nfaces_in_facet = st[1] - st[0]
+        shape = list(ar.shape)
+        if rs:
+            # we assume the other horizontal dimension is immediately after jdim
+            shape[jdim] = ar.shape[jdim+1]
+            shape[jdim+1] = ar.shape[jdim]
+        # insert a length-1 dimension along which to concatenate
+        shape.insert(jdim, 1)
+        # modify the array shape in place, no copies allowed
+        ar.shape = shape
+        # now ar is propery shaped, but we still need to slice it into faces
+        face_slice_dim = jdim + 1 + rs
+        for n in range(nfaces_in_facet):
+            face_slice = (face_slice_dim * (slice(None),) +
+                          (slice(nside*n, nside*(n+1)),))
+            data_face = ar[face_slice]
+            face_arrays.append(data_face)
+
+    # We can't concatenate using numpy (hcat etc.) because it makes a copy,
+    # presumably loading the memmaps into memory.
+    # Using dask gets around this.
+    # But what if we want different chunks, or already chunked the data
+    # upstream? Doesn't seem like this is ideal
+    # TODO: Refactor handling of dask arrays and chunking
+    #return np.concatenate(face_arrays, axis=jdim)
+    # the dask version doesn't work because of this:
+    # https://github.com/dask/dask/issues/1645
+    face_arrays_dask = [da.from_array(fa, chunks=fa.shape)
+                        for fa in face_arrays]
+    for fad in face_arrays_dask:
+        print(fad.token)
+    return da.concatenate(face_arrays_dask, axis=jdim)
