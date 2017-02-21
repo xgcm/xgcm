fixed sign bug

xgcm/test/datasets.py
@@ -27,6 +27,7 @@ import numpy as np
 #                 ni_u = 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5 ;
 # }
 
+N = 100
 datasets = {
     # the comodo example
     'nonperiodic_1d': xr.Dataset(
@@ -44,15 +45,14 @@ datasets = {
         }),
     # my own invention
     'periodic_1d': xr.Dataset(
-        coords={'XG': (['XG',], 2*np.pi/100*np.arange(0,100),
+        coords={'XG': (['XG',], 2*np.pi/N*np.arange(0,N),
                         {'axis': 'X',
                          'c_grid_axis_shift': -0.5}),
-                'XC': (['XC',], 2*np.pi/100*(np.arange(0,100)+0.005),
+                'XC': (['XC',], 2*np.pi/N*(np.arange(0,N)+0.5),
                                 {'axis': 'X'})
 
         })
 }
-
 
 @pytest.fixture(scope="module", params=datasets.keys())
 def all_datasets(request):

xgcm/test/test_grid.py
@@ -24,8 +24,10 @@ def test_interp_c_to_g_periodic(periodic_1d):
     """Interpolate from c grid to g grid."""
     ds = periodic_1d
 
-    # a linear gradient in the ni direction
     data_c = np.sin(ds['XC'])
+    # np.roll(np.arange(5), 1) --> [4, 0, 1, 2, 3]
+    # positive roll shifts left
+    data_expected = 0.5 * (data_c.values + np.roll(data_c.values, 1))
 
     grid = Grid(ds)
     data_g = grid.interp(data_c, 'X')
@@ -36,10 +38,37 @@ def test_interp_c_to_g_periodic(periodic_1d):
     assert len(data_g.XG)==len(data_g)
 
     # check that the values are right
-    np.testing.assert_allclose(
-        data_g.values,
-        0.5 * (data_c.values + np.roll(data_c.values, -1))
-    )
+    np.testing.assert_allclose(data_g.values, data_expected)
+
+    # try the same with chunks
+    data_c = np.sin(ds['XC'])
+    data_c = data_c.chunk(10)
+    data_g = grid.interp(data_c, 'X')
+    np.testing.assert_allclose(data_g.values, data_expected)
+
+def test_diff_c_to_g_periodic(periodic_1d):
+    ds = periodic_1d
+
+    # a linear gradient in the ni direction
+    data_c = np.sin(ds['XC'])
+    data_expected = data_c.values - np.roll(data_c.values, 1)
+
+    grid = Grid(ds)
+    data_g = grid.diff(data_c, 'X')
+
+    # check that the dimensions are right
+    assert data_g.dims == ('XG',)
+    xr.testing.assert_equal(data_g.XG, ds.XG)
+    assert len(data_g.XG)==len(data_g)
+
+    # check that the values are right
+    np.testing.assert_allclose(data_g.values, data_expected)
+
+    # try the same with chunks
+    data_c = np.sin(ds['XC'])
+    data_c = data_c.chunk(10)
+    data_g = grid.diff(data_c, 'X')
+    np.testing.assert_allclose(data_g.values, data_expected)
 
 def test_interp_g_to_c_periodic(periodic_1d):
     """Interpolate from c grid to g grid."""
@@ -47,6 +76,7 @@ def test_interp_g_to_c_periodic(periodic_1d):
 
     # a linear gradient in the ni direction
     data_g = np.sin(ds['XG'])
+    data_expected = 0.5 * (data_g.values + np.roll(data_g.values, -1))
 
     grid = Grid(ds)
     data_c = grid.interp(data_g, 'X')
@@ -57,10 +87,28 @@ def test_interp_g_to_c_periodic(periodic_1d):
     assert len(data_c.XC)==len(data_c)
 
     # check that the values are right
-    np.testing.assert_allclose(
-        data_c.values,
-        0.5 * (data_g.values + np.roll(data_g.values, 1))
-    )
+    np.testing.assert_allclose(data_c.values, data_expected)
+
+def test_diff_g_to_c_periodic(periodic_1d):
+    ds = periodic_1d
+
+    # a linear gradient in the ni direction
+    data_g = np.sin(ds['XG'])
+    # np.roll(np.arange(5), -1) --> [1, 2, 3, 4, 0]
+    # negative roll shifts right
+    data_expected = np.roll(data_g.values, -1) - data_g.values
+    #data_expected = np.cos(ds['XC']).values * (2*np.pi) / 100.
+
+    grid = Grid(ds)
+    data_c = grid.diff(data_g, 'X')
+
+    # check that the dimensions are right
+    assert data_c.dims == ('XC',)
+    xr.testing.assert_equal(data_c.XC, ds.XC)
+    assert len(data_c.XC)==len(data_c)
+
+    # check that the values are right
+    np.testing.assert_allclose(data_c.values, data_expected)
 
 def test_interp_c_to_g_nonperiodic(nonperiodic_1d):
     """Interpolate from c grid to g grid."""
@@ -70,6 +118,7 @@ def test_interp_c_to_g_nonperiodic(nonperiodic_1d):
     # a linear gradient in the ni direction
     grad = 0.24
     data_c = grad * ds['ni']
+    data_expected = 0.5 * (data_c.values[1:] + data_c.values[:-1])
 
     grid = Grid(ds, x_periodic=False)
     data_u = grid.interp(data_c, 'X')
@@ -80,10 +129,28 @@ def test_interp_c_to_g_nonperiodic(nonperiodic_1d):
     assert len(data_u.ni_u)==len(data_u)
 
     # check that the values are right
-    np.testing.assert_allclose(
-        data_u.values,
-        0.5 * (data_c.values[1:] + data_c.values[:-1])
-    )
+    np.testing.assert_allclose(data_u.values, data_expected)
+
+
+def test_diff_c_to_g_nonperiodic(nonperiodic_1d):
+    ds = nonperiodic_1d
+
+    # a linear gradient in the ni direction
+    grad = 0.24
+    data_c = grad * ds['ni']
+    data_expected = data_c.values[1:] - data_c.values[:-1]
+
+    grid = Grid(ds, x_periodic=False)
+    data_u = grid.diff(data_c, 'X')
+
+    # check that the dimensions are right
+    assert data_u.dims == ('ni_u',)
+    xr.testing.assert_equal(data_u.ni_u, ds.ni_u[1:-1])
+    assert len(data_u.ni_u)==len(data_u)
+
+    # check that the values are right
+    np.testing.assert_allclose(data_u.values, data_expected)
+    np.testing.assert_allclose(data_u.values, grad)
 
 def test_interp_g_to_c_nonperiodic(nonperiodic_1d):
     """Interpolate from g grid to c grid."""
@@ -93,6 +160,7 @@ def test_interp_g_to_c_nonperiodic(nonperiodic_1d):
     # a linear gradient in the ni direction
     grad = 0.43
     data_u = grad * ds['ni_u']
+    data_expected = 0.5 * (data_u.values[1:] + data_u.values[:-1])
 
     grid = Grid(ds, x_periodic=False)
     data_c = grid.interp(data_u, 'X')
@@ -103,7 +171,27 @@ def test_interp_g_to_c_nonperiodic(nonperiodic_1d):
     assert len(data_c.ni)==len(data_c)
 
     # check that the values are right
-    np.testing.assert_allclose(
-        data_c.values,
-        0.5 * (data_u.values[1:] + data_u.values[:-1])
-    )
+    np.testing.assert_allclose(data_c.values, data_expected)
+
+
+def test_diff_g_to_c_nonperiodic(nonperiodic_1d):
+    """Interpolate from g grid to c grid."""
+
+    ds = nonperiodic_1d
+
+    # a linear gradient in the ni direction
+    grad = 0.43
+    data_u = grad * ds['ni_u']
+    data_expected = data_u.values[1:] - data_u.values[:-1]
+
+    grid = Grid(ds, x_periodic=False)
+    data_c = grid.diff(data_u, 'X')
+
+    # check that the dimensions are right
+    assert data_c.dims == ('ni',)
+    xr.testing.assert_equal(data_c.ni, ds.ni)
+    assert len(data_c.ni)==len(data_c)
+
+    # check that the values are right
+    np.testing.assert_allclose(data_c.values, data_expected)
+    np.testing.assert_allclose(data_c.values, grad)