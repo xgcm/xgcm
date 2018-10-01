ae20a83b8234d10aaf490b047ef65786a35fd69c
stickler-ci <support@stickler-ci.com>
2018-10-01 13:53:54 +0000

    Fixing style errors.

--- xgcm/test/test_llc.py
+++ xgcm/test/test_llc.py
@@ -8,6 +8,7 @@ import pytest
 
 from xgcm.grid import Grid
 
+
 @pytest.fixture(scope="module",)
 def llc_ds(request):
     shape = (13, 90, 90)
@@ -22,40 +23,42 @@ def llc_ds(request):
                      'i_g': ('i_g', np.arange(nx))})
     return ds
 
+
 LLC_COORDS = {'X': {'center': 'i', 'left': 'i_g'},
               'Y': {'center': 'j', 'left': 'j_g'}}
 
 LLC_FACE_CONNECTIONS = {'face': {0: {'X': ((12, 'Y', False), (3, 'X', False)),
-                                   'Y': (None, (1, 'Y', False))},
-                               1: {'X': ((11, 'Y', False), (4, 'X', False)),
-                                   'Y': ((0, 'Y', False), (2, 'Y', False))},
-                               2: {'X': ((10, 'Y', False), (5, 'X', False)),
-                                   'Y': ((1, 'Y', False), (6, 'X', False))},
-                               3: {'X': ((0, 'X', False), (9, 'Y', False)),
-                                   'Y': (None, (4, 'Y', False))},
-                               4: {'X': ((1, 'X', False), (8, 'Y', False)),
-                                   'Y': ((3, 'Y', False), (5, 'Y', False))},
-                               5: {'X': ((2, 'X', False), (7, 'Y', False)),
-                                   'Y': ((4, 'Y', False), (6, 'Y', False))},
-                               6: {'X': ((2, 'Y', False), (7, 'X', False)),
-                                   'Y': ((5, 'Y', False), (10, 'X', False))},
-                               7: {'X': ((6, 'X', False), (8, 'X', False)),
-                                   'Y': ((5, 'X', False), (10, 'Y', False))},
-                               8: {'X': ((7, 'X', False), (9, 'X', False)),
-                                   'Y': ((4, 'X', False), (11, 'Y', False))},
-                               9: {'X': ((8, 'X', False), None),
-                                   'Y': ((3, 'X', False), (12, 'Y', False))},
-                               10: {'X': ((6, 'Y', False), (11, 'X', False)),
-                                    'Y': ((7, 'Y', False), (2, 'X', False))},
-                               11: {'X': ((10, 'X', False), (12, 'X', False)),
-                                    'Y': ((8, 'Y', False), (1, 'X', False))},
-                               12: {'X': ((11, 'X', False), None),
-                                    'Y': ((9, 'Y', False), (0, 'X', False))}}}
+                                     'Y': (None, (1, 'Y', False))},
+                                 1: {'X': ((11, 'Y', False), (4, 'X', False)),
+                                     'Y': ((0, 'Y', False), (2, 'Y', False))},
+                                 2: {'X': ((10, 'Y', False), (5, 'X', False)),
+                                     'Y': ((1, 'Y', False), (6, 'X', False))},
+                                 3: {'X': ((0, 'X', False), (9, 'Y', False)),
+                                     'Y': (None, (4, 'Y', False))},
+                                 4: {'X': ((1, 'X', False), (8, 'Y', False)),
+                                     'Y': ((3, 'Y', False), (5, 'Y', False))},
+                                 5: {'X': ((2, 'X', False), (7, 'Y', False)),
+                                     'Y': ((4, 'Y', False), (6, 'Y', False))},
+                                 6: {'X': ((2, 'Y', False), (7, 'X', False)),
+                                     'Y': ((5, 'Y', False), (10, 'X', False))},
+                                 7: {'X': ((6, 'X', False), (8, 'X', False)),
+                                     'Y': ((5, 'X', False), (10, 'Y', False))},
+                                 8: {'X': ((7, 'X', False), (9, 'X', False)),
+                                     'Y': ((4, 'X', False), (11, 'Y', False))},
+                                 9: {'X': ((8, 'X', False), None),
+                                     'Y': ((3, 'X', False), (12, 'Y', False))},
+                                 10: {'X': ((6, 'Y', False), (11, 'X', False)),
+                                      'Y': ((7, 'Y', False), (2, 'X', False))},
+                                 11: {'X': ((10, 'X', False), (12, 'X', False)),
+                                      'Y': ((8, 'Y', False), (1, 'X', False))},
+                                 12: {'X': ((11, 'X', False), None),
+                                      'Y': ((9, 'Y', False), (0, 'X', False))}}}
 
 
 @pytest.fixture(scope="module")
 def llc_grid(llc_ds):
-    grid = Grid(llc_ds, coords=LLC_COORDS, face_connections=LLC_FACE_CONNECTIONS)
+    grid = Grid(llc_ds, coords=LLC_COORDS,
+                face_connections=LLC_FACE_CONNECTIONS)
     return grid
 
 
