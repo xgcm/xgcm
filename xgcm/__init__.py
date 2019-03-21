ad41f08eccc9375fbf12d70585e1c55bfc471f86
Ryan Abernathey <ryan.abernathey@gmail.com>
2019-03-20 21:00:00 -0400

    Switch to versioneer (#123)
    * add versioneer
    
    * remove bumpversion
    
    * remove stickler too
    
    * add python 3.6 test environment
    
    * exclude versioneer stuff from coverage

--- xgcm/__init__.py
+++ xgcm/__init__.py
@@ -1,3 +1,6 @@
-__version__ = "0.1.0"
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 from .grid import Grid, Axis
 from .autogenerate import generate_grid_ds
