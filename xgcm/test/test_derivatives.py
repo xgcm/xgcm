fa3732372b1a80420c1e066eda26a993e48962e5
Timothy Smith <timsmith204@utexas.edu>
2019-07-05 23:26:36 +0200

    simple addition to uniform derivative test

--- xgcm/test/test_derivatives.py
+++ xgcm/test/test_derivatives.py
@@ -8,23 +8,41 @@ def test_derivative_uniform_grid():
     # this is a uniform grid
     # a non-uniform grid would provide a more rigorous test
     dx = 10.0
+    dy = 10.0
+    arr = [[1.0, 2.0, 4.0, 3.0], \
+           [4.0, 7.0, 1.0, 2.0], \
+           [3.0, 1.0, 0.0, 9.0], \
+           [8.0, 5.0, 2.0, 1.0]]
     ds = xr.Dataset(
-        {"foo": (("XC",), [1.0, 2.0, 4.0, 3.0])},
+        {"foo": (("XC","YC"), arr)},
         coords={
             "XC": (("XC",), [0.5, 1.5, 2.5, 3.5]),
             "XG": (("XG",), [0, 1.0, 2.0, 3.0]),
             "dXC": (("XC",), [dx, dx, dx, dx]),
             "dXG": (("XG",), [dx, dx, dx, dx]),
+            "YC": (("YC",), [0.5, 1.5, 2.5, 3.5]),
+            "YG": (("YG",), [0, 1.0, 2.0, 3.0]),
+            "dYC": (("YC",), [dy, dy, dy, dy]),
+            "dYG": (("YG",), [dy, dy, dy, dy]),
         },
     )
 
     grid = Grid(
         ds,
-        coords={"X": {"center": "XC", "left": "XG"}},
-        metrics={("X",): ["dXC", "dXG"]},
+        coords={"X": {"center": "XC", "left": "XG"},
+                "Y": {"center": "YC", "left": "YG"}},
+        metrics={("X",): ["dXC", "dXG"],
+                 ("Y",): ["dYC", "dYG"]},
         periodic=True,
     )
 
+    # Test x direction
     dfoo_dx = grid.derivative(ds.foo, "X")
     expected = grid.diff(ds.foo, "X") / dx
     assert dfoo_dx.equals(expected)
+
+    # Test x direction
+    dfoo_dy = grid.derivative(ds.foo, "Y")
+    expected = grid.diff(ds.foo, "Y") / dy
+    assert dfoo_dy.equals(expected)
+
