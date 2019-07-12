b16a92c0348064ec37810b6242ac236d910f2281
Timothy Smith <timsmith204@utexas.edu>
2019-07-12 16:50:00 +0200

    black reformatting: c grid derivative test

--- xgcm/test/test_derivatives.py
+++ xgcm/test/test_derivatives.py
@@ -6,6 +6,7 @@ import numpy as np
 from xgcm.grid import Grid, Axis
 from xgcm.test.test_metrics import datasets
 
+
 def test_derivative_uniform_grid():
     # this is a uniform grid
     # a non-uniform grid would provide a more rigorous test
@@ -41,8 +42,6 @@ def test_derivative_uniform_grid():
         periodic=True,
     )
 
-
-
     # Test x direction
     dfoo_dx = grid.derivative(ds.foo, "X")
     expected = grid.diff(ds.foo, "X") / dx
@@ -53,50 +52,51 @@ def test_derivative_uniform_grid():
     expected = grid.diff(ds.foo, "Y") / dy
     assert dfoo_dy.equals(expected)
 
+
 def test_derivative_c_grid():
     # test derivatives with synthetic C grid data
     # using test_metrics.datasets()
 
     ds_full = datasets()
-    ds = ds_full['C']
-    grid = Grid(ds, coords=ds_full['coords'], metrics=ds_full['metrics'])
+    ds = ds_full["C"]
+    grid = Grid(ds, coords=ds_full["coords"], metrics=ds_full["metrics"])
 
     # run this for each axis and each field in dataset
     def test_single_derivative(axis, fld, dx):
 
         dvar_dx = grid.derivative(fld, axis)
         expected = grid.diff(fld, axis) / dx
-        abs_diff = np.sum( np.abs( dvar_dx - expected ) )
+        abs_diff = np.sum(np.abs(dvar_dx - expected))
 
         # for some reason .equals() returns false
         # even though absolute value of difference is zero
-        assert abs_diff == 0.
-        #assert dvar_dx.equals(expected)
+        assert abs_diff == 0.0
+        # assert dvar_dx.equals(expected)
 
     # tracer point
-    var = 'tracer'
-    test_axes = ['X','Y','Z']
-    test_dx = ['dx_e','dy_n','dz_w']
+    var = "tracer"
+    test_axes = ["X", "Y", "Z"]
+    test_dx = ["dx_e", "dy_n", "dz_w"]
     for ax, dx in zip(test_axes, test_dx):
         test_single_derivative(ax, ds[var], ds[dx])
 
     # zonal velocity point
-    var = 'u'
-    test_axes = ['X','Y']
-    test_dx = ['dx_t','dy_ne']
+    var = "u"
+    test_axes = ["X", "Y"]
+    test_dx = ["dx_t", "dy_ne"]
     for ax, dx in zip(test_axes, test_dx):
         test_single_derivative(ax, ds[var], ds[dx])
 
     # meridional velocity point
-    var = 'v'
-    test_axes = ['X','Y']
-    test_dx = ['dx_ne','dy_t']
+    var = "v"
+    test_axes = ["X", "Y"]
+    test_dx = ["dx_ne", "dy_t"]
     for ax, dx in zip(test_axes, test_dx):
         test_single_derivative(ax, ds[var], ds[dx])
 
     # vertical velocity point
-    var = 'wt'
-    test_axes = ['X','Y','Z']
-    test_dx = ['dx_e','dy_n','dz_t']
+    var = "wt"
+    test_axes = ["X", "Y", "Z"]
+    test_dx = ["dx_e", "dy_n", "dz_t"]
     for ax, dx in zip(test_axes, test_dx):
         test_single_derivative(ax, ds[var], ds[dx])
