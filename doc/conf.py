adad4460361cde787e9799fd107149d3e2d4bbc9
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-02-15 12:36:34 -0800

    fix autogenerate nb execution

--- doc/conf.py
+++ doc/conf.py
@@ -49,7 +49,8 @@ extensions = [
 
 # never execute notebooks: avoids lots of expensive imports on rtd
 # https://nbsphinx.readthedocs.io/en/0.2.14/never-execute.html
-#nbsphinx_execute = 'never'
+# nbsphinx_execute = 'never'
+exclude_patterns = ["_build", ".ipynb_checkpoints"]
 
 # http://stackoverflow.com/questions/5599254/how-to-use-sphinxs-autodoc-to-document-a-classs-init-self-method
 def skip(app, what, name, obj, skip, options):
@@ -106,7 +107,6 @@ version = ".".join(release.split(".")[:2])
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
