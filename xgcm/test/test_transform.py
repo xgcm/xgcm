6c2dbe925234c58ea9f9b1b925e9cf8ba102024d
jbusecke <julius@ldeo.columbia.edu>
2020-09-08 17:10:41 -0400

    Added modules back

---
+++ xgcm/test/test_transform.py
@@ -0,0 +1,155 @@
+"""Tests for low transform logic."""
+
+import pytest
+
+import numpy as np
+import xarray as xr
+from ..transform import interp_1d_linear, interp_1d_conservative, linear_interpolation
+
+from xgcm.grid import Grid, Axis
+
+"""Tests for low level routines."""
+
+
+def test_interp_1d_linear():
+    nz, nx = 100, 1000
+    z_vertex = np.linspace(0, 1, nz + 1)
+    z = 0.5 * (z_vertex[:-1] + z_vertex[1:])
+    x = 2 * np.pi * np.linspace(0, 1, nx)
+    # uniformly stratified scalar
+    theta = z + 0.1 * np.cos(3 * x)[:, None]
+    # the scalar to interpolate
+    phi = np.sin(theta) + 0.1 * np.cos(5 * x)[:, None]
+    target_theta_levels = np.arange(0.2, 0.9, 0.025)
+    phi_at_theta_expected = np.sin(target_theta_levels) + 0.1 * np.cos(5 * x)[:, None]
+
+    # the action
+    phi_at_theta = interp_1d_linear(phi, theta, target_theta_levels, mask_edges=False)
+    np.testing.assert_allclose(phi_at_theta, phi_at_theta_expected, rtol=1e-4)
+
+
+def test_interp_1d_conservative():
+    nz = 30
+    k = np.arange(nz)
+    dz = 10 + np.linspace(0, 90, nz - 1)
+    z = np.concatenate([[0], np.cumsum(dz)])
+    H = z.max()
+    theta = z / H + 0.2 * np.cos(np.pi * z / H)
+    # phi = np.sin(5 * np.pi * z/H)
+
+    nbins = 100
+    theta_bins = np.linspace(theta.min() - 0.1, theta.max() + 0.1, nbins)
+
+    # lazy way to check that it vectorizes: just copy the 1d array
+    nx = 5
+    dz_2d = np.tile(dz, (nx, 1))
+    theta_2d = np.tile(theta, (nx, 1))
+
+    dz_theta = interp_1d_conservative(dz_2d, theta_2d, theta_bins)
+
+    np.testing.assert_allclose(dz_theta.sum(axis=-1), dz.sum(axis=-1))
+
+
+"""1D Test datasets for various transformations."""
+
+# TODO: I am not sure how we would handle periodic axes atm. Should we raise an error?
+def raw_datasets():
+    z = np.array([5, 25, 60])
+    z_outer = np.array([0, 10, 50, 75])
+
+    #####
+    # First set of examples: Linear interpolation on different depth levels
+    target_z = np.array([0, 7, 30, 60, 70])
+    data = np.random.rand(len(z))
+
+    data_expected = np.interp(target_z, z, data)
+
+    input_linear_depth_depth = xr.Dataset(
+        {"data": xr.DataArray(data, dims=["z"], coords={"z": z})}
+    )
+    expected_linear_depth_depth = xr.DataArray(
+        data_expected, dims=["z"], coords={"z": target_z}
+    )
+
+    # same with masked outcrops
+    data_expected_masked_outcrops = data_expected.copy()
+    data_expected_masked_outcrops[0] = np.nan
+    data_expected_masked_outcrops[-1] = np.nan
+    expected_linear_depth_depth_masked_outcrops = xr.DataArray(
+        data_expected_masked_outcrops, dims=["z"], coords={"z": target_z}
+    )
+    #####
+    # Second set of examples: Conservative remapping to other depth levels.
+
+    datasets = {
+        "linear_depth_depth": (
+            input_linear_depth_depth,
+            {"coords": {"Z": {"center": "z"}}},
+            target_z,
+            {"mask_edges": False},
+            expected_linear_depth_depth,
+        ),
+        "linear_depth_depth_renamed": (
+            input_linear_depth_depth.rename({"z": "test"}),
+            {"coords": {"Z": {"center": "test"}}},
+            target_z,
+            {"mask_edges": True},
+            expected_linear_depth_depth_masked_outcrops.rename({"z": "test"}),
+        ),
+        "linear_depth_depth_masked_outcrops": (
+            input_linear_depth_depth,
+            {"coords": {"Z": {"center": "z"}}},
+            target_z,
+            {"mask_edges": True},
+            expected_linear_depth_depth_masked_outcrops,
+        ),
+    }
+    return datasets
+
+
+@pytest.fixture(
+    scope="module",
+    params=[
+        "linear_depth_depth",
+        "linear_depth_depth_masked_outcrops",
+        "linear_depth_depth_renamed",
+    ],
+)
+def dataset(request):
+    input, grid_kwargs, target, transform_kwargs, expected = raw_datasets[request.param]
+    return input, grid_kwargs, target, transform_kwargs, expected
+
+
+"""Test suite."""
+
+
+def test_linear_interpolation_target_value_error():
+    """Test that linear_interpolation throws an error when `target` is a np array"""
+    input, grid_kwargs, target, transform_kwargs, expected = raw_datasets()[
+        "linear_depth_depth"
+    ]
+    with pytest.raises(ValueError):
+        interpolated = linear_interpolation(input.data, input["z"], target, "z", "z")
+
+
+def test_low_level_interpolate(dataset):
+    input, grid_kwargs, target, transform_kwargs, expected = dataset
+    dim = grid_kwargs["coords"]["Z"]["center"]
+    da_target = xr.DataArray(target, dims=[dim], coords={dim: target})
+    print(transform_kwargs)
+    interpolated = linear_interpolation(
+        input.data, input[dim], da_target, dim, dim, **transform_kwargs
+    )
+    xr.testing.assert_allclose(interpolated, expected)
+
+
+def test_grid_transform(dataset):
+    input, grid_kwargs, target, transform_kwargs, expected = dataset
+
+    axis = list(grid_kwargs["coords"].keys())[0]
+    dim = grid_kwargs["coords"][axis]["center"]
+    da_target = xr.DataArray(target, dims=[dim], coords={dim: target})
+
+    grid = Grid(input, **grid_kwargs)
+    transformed = grid.transform(input.data, axis, da_target, **transform_kwargs)
+    xr.testing.assert_allclose(transformed, expected)
---
+++ xgcm/transform.py
@@ -0,0 +1,163 @@
+"""
+Classes and functions for 1D coordinate transformation.
+"""
+
+import numpy as np
+import xarray as xr
+from numba import jit, guvectorize, float32, float64, boolean
+
+
+@guvectorize(
+    [
+        (float64[:], float64[:], float64[:], boolean, float64[:]),
+        (float32[:], float32[:], float32[:], boolean, float32[:]),
+    ],
+    "(n),(n),(m),()->(m)",
+    nopython=True,
+)
+def _interp_1d_linear(phi, theta, target_theta_levels, mask_edges, output):
+    output[:] = np.interp(target_theta_levels, theta, phi)
+
+    if mask_edges:
+        theta_max = np.nanmax(theta)
+        theta_min = np.nanmin(theta)
+        for i in range(len(target_theta_levels)):
+            theta_lev = target_theta_levels[i]
+            if (theta_lev < theta_min) or (theta_lev > theta_max):
+                output[i] = np.nan
+
+
+def interp_1d_linear(phi, theta, target_theta_levels, mask_edges=False):
+    """
+    Vectorized interpolation of scalar phi to isosurfaces of scalar theta
+    along the final axis.
+
+    Parameters
+    ----------
+    phi : array_like
+        Array of shape (..., n), scalar field to be interpolated
+    theta : array_like
+        Array of shape (..., n), scalar field which defines the isosurfaces
+    target_theta_levels : array_like
+        Array of shape (m) specificying target isosurface levels
+    mask_edges : bool, optional
+        Determines how to handle theta values that exceed the bounds of
+        target_theta_levels. If False, fill with nearest valid values. If
+        True, fill with NaNs.
+
+    Returns
+    -------
+    phi_interp : array
+        Array of shape (..., m) of phi interpolated to theta isosurfaces.
+    """
+    return _interp_1d_linear(phi, theta, target_theta_levels, mask_edges)
+
+
+@guvectorize(
+    [
+        (float64[:], float64[:], float64[:], float64[:], float64[:], float64[:]),
+        (float32[:], float32[:], float32[:], float32[:], float32[:], float32[:]),
+    ],
+    "(n),(n),(n),(m),(m)->(m)",
+    nopython=True,
+)
+def _interp_1d_conservative(phi, theta_1, theta_2, theta_hat_1, theta_hat_2, output):
+    output[:] = 0
+
+    n = len(theta_1)
+    m = len(theta_hat_1)
+
+    for i in range(n):
+        # handle non-monotonic stratification
+        if theta_1[i] < theta_2[i]:
+            theta_min = theta_1[i]
+            theta_max = theta_2[i]
+        else:
+            theta_min = theta_2[i]
+            theta_max = theta_1[i]
+
+        for j in range(m):
+            if (theta_hat_1[j] > theta_max) or (theta_hat_2[j] < theta_min):
+                # there is no overlap between the cell and the bin
+                pass
+            else:
+                # from here on there is some overlap
+                theta_hat_min = max(theta_min, theta_hat_1[j])
+                theta_hat_max = min(theta_max, theta_hat_2[j])
+                alpha = (theta_hat_max - theta_hat_min) / (theta_max - theta_min)
+                # now assign based on this weight
+                output[j] += alpha * phi[i]
+
+
+def interp_1d_conservative(phi, theta, target_theta_bins):
+    """
+    Accumulate extensive cell-centered quantity phi into new vertical coordinate
+    defined by scalar theta.
+
+    Parameters
+    ----------
+    phi : array_like
+        Array of shape (..., n) defining an extensive quanitity in a cell
+        bounded by two vertices.
+    theta : array_like
+        Array of shape (..., n+1) giving values of scalar theta  on the
+        cell vertices. Phi is assumed to vary linearly between vertices.
+    target_theta_bins : array_like
+        Array of shape (m) defining the bounds of bins in which to accumulate
+        phi.
+
+    Returns
+    -------
+    phi_accum : array_like
+        Array of shape (..., m-1) giving the values of phi partitioned into
+        specified theta bins.
+    """
+
+    assert phi.shape[-1] == (theta.shape[-1] - 1)
+    assert target_theta_bins.ndim == 1
+    assert all(np.diff(target_theta_bins) > 0)
+
+    theta_1 = theta[..., :-1]
+    theta_2 = theta[..., 1:]
+    theta_hat_1 = target_theta_bins[:-1]
+    theta_hat_2 = target_theta_bins[1:]
+
+    return _interp_1d_conservative(phi, theta_1, theta_2, theta_hat_1, theta_hat_2)
+
+
+"""
+Higher level functions (xarray wrappers).
+"""
+
+
+def linear_interpolation(phi, theta, target_theta_levels, dim, target_dim, **kwargs):
+    # complain if the target values are not provided as dataarray
+    if not isinstance(target_theta_levels, xr.DataArray):
+        raise ValueError("`target_theta_levels` should be passed as xr.DataArray")
+
+    # if `dim` and `target_dim` are the same
+    # (e.g. simple interpoaltion to different depths)
+    # the dimension needs to be temporarily renamed
+    rename_trigger = False
+    if dim == target_dim:
+        saved_dim = target_dim
+        temp_dim = "temp_dim_xyz"
+        target_theta_levels = target_theta_levels.rename({target_dim: temp_dim})
+        target_dim = temp_dim
+        rename_trigger = True
+
+    out = xr.apply_ufunc(
+        interp_1d_linear,
+        phi,
+        theta,
+        target_theta_levels,
+        kwargs=kwargs,
+        input_core_dims=[[dim], [dim], [target_dim]],
+        output_core_dims=[[target_dim]],
+        dask="parallelized",
+        output_dtypes=[phi.dtype],
+    )
+
+    if rename_trigger:
+        out = out.rename({temp_dim: saved_dim})
+    return out
