aa6f2afb5a25e712ea889e21cfeef79883c60d4b
jbusecke <julius@ldeo.columbia.edu>
2020-10-09 09:21:27 -0400

    Added failing test for nan in target_data

--- xgcm/test/test_transform.py
+++ xgcm/test/test_transform.py
@@ -125,8 +125,6 @@ cases = {
         },
     },
     # example of interpolating onto a tracer that descreases with depth
-    # This fails due to the temp not increasing. We should implement a heuristic
-    # to switch the direction...
     "linear_depth_temp": {
         "source_coord": ("depth", [20, 40, 60, 80, 100, 120]),
         "source_data": ("data", [-3, 0, 2, 6, 4, 1]),
@@ -138,6 +136,27 @@ cases = {
         "expected_data": (
             "data",
             [1.0, 4.0, 6.0, 5.6, 4.0, 2.0, -0.272727, -0.818182],
+        ),
+        "grid_kwargs": {"coords": {"Z": {"center": "depth", "outer": "depth_bnds"}}},
+        # "error": True,  # this currently fails but shouldnt
+        "transform_kwargs": {
+            "method": "linear",
+            "target_data": "temp",
+            "mask_edges": False,
+        },
+    },
+    # example of interpolating onto a tracer that descreases with depth
+    "linear_depth_temp_missing_values": {
+        "source_coord": ("depth", [20, 40, 60, 80, 100, 120]),
+        "source_data": ("data", [-3, 0, 2, 6, 4, 1]),
+        "source_additional_data_coord": ("depth", [20, 40, 60, 80, 100, 120]),
+        "source_additional_data": ("temp", [np.nan, 24, 20, 10, 5, 1]),
+        "target_coord": ("something", [0, 5, 10, 11, 15, 20]),
+        "target_data": ("something", [0, 5, 10, 11, 15, 20]),
+        "expected_coord": ("something", [0, 5, 10, 11, 15, 20]),
+        "expected_data": (
+            "data",
+            [1.0, 4.0, 6.0, 5.6, 4.0, 2.0],
         ),
         "grid_kwargs": {"coords": {"Z": {"center": "depth", "outer": "depth_bnds"}}},
         # "error": True,  # this currently fails but shouldnt
