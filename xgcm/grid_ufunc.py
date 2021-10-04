e2df0419da815625d1ec8c9c93280fc54d682fcd
Thomas Nicholas <thomas.nicholas@columbia.edu>
2021-10-04 16:52:53 -0400

    drop non-dimension coords

--- xgcm/grid_ufunc.py
+++ xgcm/grid_ufunc.py
@@ -195,6 +195,7 @@ def apply_as_grid_ufunc(
     boundary_width=None,
     boundary=None,
     fill_value=None,
+    keep_coords=True,
     dask="forbidden",
     **kwargs,
 ):
@@ -375,8 +376,7 @@ def apply_as_grid_ufunc(
     if not isinstance(results, tuple):
         results = (results,)
 
-    # Restore any coordinates associated with new output dims that are present in grid
-    # TODO should this be optional via a `keep_coords` arg?
+    # Restore any dimension coordinates associated with new output dims that are present in grid
     results_with_coords = []
     for res, arg_out_core_dims in zip(results, out_core_dims):
         new_core_dim_coords = {
@@ -396,6 +396,13 @@ def apply_as_grid_ufunc(
                 )
             else:
                 raise
+
+        if not keep_coords:
+            # TODO I don't like the `keep_coords` argument in general and think it should be removed for clarity.
+            # Drop any non-dimension coordinates on the output
+            non_dim_coords = [coord for coord in res.coords if coord not in res.dims]
+            res = res.drop_vars(non_dim_coords)
+
         results_with_coords.append(res)
 
     # Return single results not wrapped in 1-element tuple, like xr.apply_ufunc does
