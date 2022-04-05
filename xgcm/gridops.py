e75ad878c8111e5ab0ba8c0dd46f2c828402537a
Thomas Nicholas <thomas.nicholas@columbia.edu>
2022-04-05 11:39:31 -0400

    add cumsum gridops

--- xgcm/gridops.py
+++ xgcm/gridops.py
@@ -17,6 +17,9 @@ through when `xgcm.Grid.diff()` is called. See xgcm.grid_ufunc._select_grid_ufun
 # TODO can we allow for grouping these definitions into classes? Similar to pytest tests?
 
 
+# Diff
+
+
 def diff_forward(a):
     return a[..., 1:] - a[..., :-1]
 
@@ -68,6 +71,9 @@ def diff_left_to_inner(a):
     raise NotImplementedError
 
 
+# Interp
+
+
 def interp_forward(a):
     return (a[..., :-1] + a[..., 1:]) / 2.0
 
@@ -112,7 +118,7 @@ def interp_inner_to_center(a):
     return interp_forward(a)
 
 
-# TODO ufuncs for max, cumsum
+# Min
 
 
 def pairwise_forward_min(a):
@@ -161,6 +167,9 @@ def min_inner_to_center(a):
     return pairwise_forward_min(a)
 
 
+# Max
+
+
 def pairwise_forward_max(a):
     left, right = a[..., :-1], a[..., 1:]
     stacked_pairs = np.stack([left, right], axis=-1)
@@ -205,3 +214,67 @@ def max_center_to_inner(a):
 @as_grid_ufunc(signature="(X:inner)->(X:center)", boundary_width={"X": (1, 1)})
 def max_inner_to_center(a):
     return pairwise_forward_max(a)
+
+
+# Cumsum
+
+
+@as_grid_ufunc(
+    signature="(X:center)->(X:left)",
+    boundary_width={"X": (1, 0)},
+    fill_value=0,
+    pad_before_func=False,
+)
+def cumsum_center_to_left(a):
+    # TODO will this dispatch to dask.array.cumsum if it encounters a dask array?
+    return np.cumsum(a, axis=-1)[..., :-1]
+
+
+@as_grid_ufunc(signature="(X:left)->(X:center)", boundary_width={"X": (0, 0)})
+def cumsum_left_to_center(a):
+    return np.cumsum(a, axis=-1)
+
+
+@as_grid_ufunc(signature="(X:center)->(X:right)", boundary_width={"X": (0, 0)})
+def cumsum_center_to_right(a):
+    return np.cumsum(a, axis=-1)
+
+
+@as_grid_ufunc(
+    signature="(X:right)->(X:center)",
+    boundary_width={"X": (1, 0)},
+    fill_value=0,
+    pad_before_func=False,
+)
+def cumsum_right_to_center(a):
+    return np.cumsum(a, axis=-1)[..., :-1]
+
+
+@as_grid_ufunc(
+    signature="(X:center)->(X:outer)",
+    boundary_width={"X": (1, 0)},
+    fill_value=0,
+    pad_before_func=False,
+)
+def cumsum_center_to_outer(a):
+    return np.cumsum(a, axis=-1)
+
+
+@as_grid_ufunc(signature="(X:outer)->(X:center)", boundary_width={"X": (0, 0)})
+def cumsum_outer_to_center(a):
+    return np.cumsum(a, axis=-1)[..., :-1]
+
+
+@as_grid_ufunc(signature="(X:center)->(X:inner)", boundary_width={"X": (0, 0)})
+def cumsum_center_to_inner(a):
+    return np.cumsum(a, axis=-1)[..., :-1]
+
+
+@as_grid_ufunc(
+    signature="(X:inner)->(X:center)",
+    boundary_width={"X": (1, 0)},
+    fill_value=0,
+    pad_before_func=False,
+)
+def cumsum_inner_to_center(a):
+    return np.cumsum(a, axis=-1)
