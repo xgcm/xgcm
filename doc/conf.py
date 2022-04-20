f4d93978627aff826f7f668f7491c7ba7050d757
Julius Busecke <julius@ldeo.columbia.edu>
2022-04-20 16:20:01 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -212,7 +212,7 @@ html_static_path = ["_static"]
 # html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {"index": [], "**": ["localtoc.html", "srclinks.html"]}
+# html_sidebars = {"index": [], "**": ["localtoc.html", "srclinks.html"]}
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
 # html_additional_pages = {}
