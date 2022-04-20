bb8a7b0f40ee7c8ef4174a9077cd2a5e70a1a5d1
Tom Nicholas <35968931+TomNicholas@users.noreply.github.com>
2022-04-20 16:04:48 -0400

    Apply suggestions from code review
    Co-authored-by: Julius Busecke <julius@ldeo.columbia.edu>

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(exclude=["docs", "tests", "tests.*", "docs.*"]),
     install_requires=install_requires,
