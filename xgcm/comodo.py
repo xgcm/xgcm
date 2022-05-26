7ed05ea7513c3492ebc9dc92553b7d99dc0b73ec
Dominic Orchard <dom.orchard@gmail.com>
2022-05-26 21:57:30 +0100

    correcting a silly mistake in my lifting of constants

--- xgcm/comodo.py
+++ xgcm/comodo.py
@@ -1,9 +1,9 @@
 from collections import OrderedDict
 
 # Representation of axis shifts
-axis_shift_left  =  0.5
-axis_shift_right = -0.5
-# Characterize valid shifts only
+axis_shift_left  = -0.5
+axis_shift_right = 0.5
+# Characeterizes valid shifts only
 valid_axis_shifts = [axis_shift_left, axis_shift_right]
 
 def assert_valid_comodo(ds):
