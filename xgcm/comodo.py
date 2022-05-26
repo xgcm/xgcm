c3b13a82a2d73b6f55e67a0bcc3ea967336c7b17
dominic <dom.orchard@gmail.com>
2022-05-26 22:17:41 +0100

    include 0 in the list of valid shifts

--- xgcm/comodo.py
+++ xgcm/comodo.py
@@ -1,10 +1,11 @@
 from collections import OrderedDict
 
 # Representation of axis shifts
-axis_shift_left  = -0.5
-axis_shift_right = 0.5
+axis_shift_left   = -0.5
+axis_shift_right  = 0.5
+axis_shift_center = 0
 # Characeterizes valid shifts only
-valid_axis_shifts = [axis_shift_left, axis_shift_right]
+valid_axis_shifts = [axis_shift_left, axis_shift_right, axis_shift_center]
 
 def assert_valid_comodo(ds):
     """Verify that the dataset meets comodo conventions
