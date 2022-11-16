2d33c58a7aaf4fc03b0ad07104084f6051707a96
Thomas Nicholas <thomas.nicholas@columbia.edu>
2022-11-16 16:46:18 -0500

    fix bug with chunked inputs

--- xgcm/grid_ufunc.py
+++ xgcm/grid_ufunc.py
@@ -909,7 +909,7 @@ def _pad_then_rechunk(
 
 def _is_dim_chunked(a, dim):
     # TODO this func can't handle Datasets - it will error if you check multiple variables with different chunking
-    return len(a.variable.chunksizes[dim]) > 0
+    return len(a.variable.chunksizes[dim]) > 1
 
 
 def _has_chunked_core_dims(obj: xr.DataArray, core_dims: Sequence[str]) -> bool:
