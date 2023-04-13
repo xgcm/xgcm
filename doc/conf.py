08c58c34a894a974db91133a5f29d315f4c382b1
Julius Busecke <julius@ldeo.columbia.edu>
2023-04-13 16:58:08 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -19,6 +19,16 @@ import sys
 
 import xgcm
 
+# If extensions (or modules to document with autodoc) are in another directory,
+ # add these directories to sys.path here. If the directory is relative to the
+ # documentation root, use os.path.abspath to make it absolute, like shown here.
+ # sys.path.insert(0, os.path.abspath('.'))
+ # sys.path.insert(os.path.abspath('..'))
+
+ print("python exec:", sys.executable)
+ print("sys.path:", sys.path)
+
+
 
 # -- General configuration ------------------------------------------------
 
