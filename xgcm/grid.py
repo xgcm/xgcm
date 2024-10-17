bde446da8b6a3715869f289171c278e045e8086b
Julius Busecke <julius@ldeo.columbia.edu>
2024-10-17 11:15:55 -0400

    Update grid.py

--- xgcm/grid.py
+++ xgcm/grid.py
@@ -144,7 +144,6 @@ class Grid:
 
             # Loop over input kwargs. If None and parsed alternative available
             # then replace local variable with autoparsed. If conflict raise error.
-            print(f"coords = {coords}")
             duplicates = []
             if "coords" in parsed_kwargs:
                 if coords is None:
